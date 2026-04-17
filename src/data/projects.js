2bce3e65360098fd1e1804c5f1568e67287922bb
Ricardo Gomes <ricardog.silva01@gmail.com>
2026-04-17 17:17:57 -0300

    corrigindo caminho das imagens do projeto

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,9 @@
 
 export const projects = [
-  { nome: "HTML", imagem: "/public/images/logo_html.png", descricao: "Estruturação de páginas web modernas." },
-  { nome: "CSS", imagem: "/public/images/logo_css.png", descricao: "Estilização e layouts responsivos." },
-  { nome: "PHP", imagem: "/public/images/logo_php.png", descricao: "Sistemas web com banco de dados." },
-  { nome: "JavaScript", imagem: "/public/images/logo_javascript.png", descricao: "Aplicações interativas." },
-  { nome: "Java", imagem: "/public/images/logo_java.webp", descricao: "Aplicações orientadas a objetos." },
-  { nome: "Python", imagem: "/public/images/logo_python.png", descricao: "Automação e análise de dados." }
+  { nome: "HTML", imagem: "/images/logo_html.png", descricao: "Estruturação de páginas web modernas." },
+  { nome: "CSS", imagem: "/images/logo_css.png", descricao: "Estilização e layouts responsivos." },
+  { nome: "PHP", imagem: "/images/logo_php.png", descricao: "Sistemas web com banco de dados." },
+  { nome: "JavaScript", imagem: "/images/logo_javascript.png", descricao: "Aplicações interativas." },
+  { nome: "Java", imagem: "/images/logo_java.webp", descricao: "Aplicações orientadas a objetos." },
+  { nome: "Python", imagem: "/images/logo_python.png", descricao: "Automação e análise de dados." }
 ];
